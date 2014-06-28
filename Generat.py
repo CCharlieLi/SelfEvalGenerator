4df75a758be78b6f150ef8074ab524445cf965ec
Charlie Li <CCharlieli@live.com>
2014-06-28 16:55:05 +0800

    SelfEvalGenerator
    每学期的学年鉴定、个人评定，党员学期鉴定，学年评价还在烦你吗？？？
    
    一键生成！

---
+++ Generat.py
@@ -0,0 +1,35 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys,random
+
+Appellation = u"同学 "
+
+def Generator(NameList,EList):
+	names = open(NameList)
+	sentences = open(EList)
+	Savefile = open("result.txt",'w')
+
+	sentenceList = []
+	sentenceslines = sentences.readlines()
+	for each in sentenceslines:
+		each = each.replace('\n','')
+		each = each.strip()
+		sentenceList.append(each)
+		#Savefile.write(each+"...")
+
+	nameslines = names.readlines()
+	for each in nameslines:
+		each = each.replace('\n','')
+		each = each.strip()
+		Savefile.write(each + Appellation+ ','.join(random.sample(sentenceList,15)) + u'。' +'\n\n')
+
+
+	names.close() 
+	sentences.close()
+	Savefile.close()
+
+
+if __name__ == "__main__":
+	reload(sys) 
+	sys.setdefaultencoding('gbk')
+	sys.exit(Generator("NameList.txt","EVAList.txt"))
